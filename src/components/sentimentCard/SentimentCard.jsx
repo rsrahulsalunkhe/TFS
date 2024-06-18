babe9356069a3d301f76aafce0e0fe2f77072698
Rahul Salunkhe <rsrahulsalunkhe4@gmail.com>
2024-06-18 23:27:30 +0530

    edite sentiments cards

--- src/components/sentimentCard/SentimentCard.jsx
+++ src/components/sentimentCard/SentimentCard.jsx
@@ -6,9 +6,9 @@ import banner2 from './../../assets/banner2.jpeg'
 const Price = (props) => {
   return (
     <div className='price-status'>
-        <p style={{fontSize: '14px', fontWeight: 700, color: '#606060'}}>{props.status}</p>
-        <p style={{fontSize: '18px', fontWeight: 500, color: '#191919'}}>{props.price}</p>
-        <p style={{fontSize: '16px', fontWeight: 500, color: props.color}}>{props.change}</p>
+        <span style={{fontSize: '14px', fontWeight: 700, color: '#606060'}}>{props.status}</span>
+        <span style={{fontSize: '18px', fontWeight: 500, color: '#191919', fontFamily: 'Work Sans'}}>{props.price}</span>
+        <span style={{fontSize: '16px', fontWeight: 500, color: props.color, fontFamily: 'Work Sans'}}>{props.change}</span>
     </div>
   )
 }
@@ -17,7 +17,7 @@ const Price = (props) => {
 const SentimentCard = () => {
   return (
     <div className='w-full lg:w-[32%]'>
-        <div className='p-4' style={{boxShadow: 'rgba(0, 0, 0, 0.15) 0px 2px 8px', borderRadius: '12px'}}>
+        <div className='p-3' style={{boxShadow: 'rgba(0, 0, 0, 0.15) 0px 2px 8px', borderRadius: '12px'}}>
             <div className='flex'>
                 <span style={{color: 'var(--blue)', fontWeight: 700}}>27-09-2023</span>
                 <span style={{color: 'var(--green)', fontWeight: 700}} className='ms-auto'>STEADY TO FIRM</span>
@@ -25,24 +25,24 @@ const SentimentCard = () => {
             <div className='flex mt-2'>
                 <div className='location'>
                     <p>DELHI</p>
-                    <span>RJ Line</span>
+                    <span style={{marginTop: '-5px'}}>RJ Line</span>
                 </div>
                 <div className='price'>
                     <p className='me-3'>₹5100-5200</p>
                     <span><p>+50</p></span>
                 </div>
             </div>
-            <div className='flex justify-around mt-2'>
-                <Price status='OPEN' price='5000-5100' change='812' color='#00AA08'/>
-                <Price status='Low' price='5000-5100' change='812' color='#EE0000'/>
-                <Price status='HIGH' price='5000-5100' change='812' color='#00AA08'/>
+            <div className='flex justify-around mt-3 mb-3'>
+                <Price status='OPEN' price='5000-5100' change='▲812' color='#00AA08'/>
+                <Price status='Low' price='5000-5100' change='▲812' color='#EE0000'/>
+                <Price status='HIGH' price='5000-5100' change='▲812' color='#00AA08'/>
             </div>
 
             <div className='flex mt-2'>
                 <div className='w-[5%] h-[20px]'>
                     <img className='w-full h-full' src={opn} alt="" />
                 </div>
-                <p className='w-[94%] text-left ms-2' style={{color: 'var(--black)', fontWeight: 700, lineHeight: 1.1}}>Trading Activity Very Dull in Chana and rate down</p>
+                <p className='w-[94%] text-left' style={{color: 'var(--black)', fontWeight: 700, lineHeight: 1.1}}>Trading Activity Very Dull in Chana and rate down</p>
             </div>
             <p className='text-left ms-6 mt-2' style={{color: '#1400FC', fontWeight: 700, fontSize: '14px'}}>M P AGRO OVERSEAS, DELHI</p>
         </div>
